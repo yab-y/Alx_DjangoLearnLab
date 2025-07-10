3794a8e78c00158b77b1ab3f88147352252a0eae
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-07-10 19:58:21 +0300

    register book model admin with custom list display

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')  # Columns in list view
+    list_filter = ('publication_year', 'author')            # Filter sidebar
+    search_fields = ('title', 'author')                     # Search box
