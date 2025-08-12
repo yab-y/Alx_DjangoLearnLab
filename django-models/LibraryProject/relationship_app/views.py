6fe39cee1a96f4f5f1e80326d6765014da272abb
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-08-12 17:12:59 +0300

    added FBV for book list.CBV for library detail and template

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from .models import Book, Library  # <-- Make sure this line is present
+from .models  import Library  # <-- Make sure this line is present
 
 def list_books(request):
     books = Book.objects.all()
