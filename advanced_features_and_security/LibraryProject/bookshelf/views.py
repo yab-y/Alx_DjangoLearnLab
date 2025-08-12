d751cbad03385caccb5305db8415cab22b2ebdae
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-08-13 02:38:15 +0300

    implementing security best practices in django

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Book
 from .forms import SearchForm
 from django.contrib.auth.decorators import permission_required
+from .forms import ExampleForm
+
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
