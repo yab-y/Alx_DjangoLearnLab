e340193d2fbdeb6f19fcdb464137acf0a1a8c072
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-08-12 18:23:27 +0300

    implementing user authentication django

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,18 +4,15 @@ from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView
 from .models import Book, Library
 
-# Function-based view to list all books
 def list_books(request):
     books = Book.objects.all()
     return render(request, 'relationship_app/list_books.html', {'books': books})
 
-# Class-based view to show details of a specific library
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
-# Rename to exactly "register" as checker wants
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
