e7a9d780cea0b222a52352335b1ce5cfca5dfca9
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-08-14 17:37:40 +0300

    initial commit

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -1,18 +1,3 @@
 from django.db import models
-from datetime import date
 
-# Author model
-class Author(models.Model):
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-# Book model
-class Book(models.Model):
-    title = models.CharField(max_length=200)
-    publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, related_name='books', on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title
+# Create your models here.
