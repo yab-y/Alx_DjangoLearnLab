b26b0a8d5afcea8acdcc5d62e2f9da7701985559
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-08-14 18:22:23 +0300

    add book crud api using drf generic views with permissions

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -8,9 +8,9 @@ from .views import (
 )
 
 urlpatterns = [
-    path('books/', BookListView.as_view(), name='book-list'),                # List all books
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),   # Retrieve a single book
-    path('books/create/', BookCreateView.as_view(), name='book-create'),     # Create a new book
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),  # Update a book
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),  # Delete a book
+    path('books/', BookListView.as_view(), name='book-list'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+    path('books/create/', BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
