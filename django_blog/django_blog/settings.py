3dee5feb0b0b5a2a393d170c8e09db475aeecbfc
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-08-14 23:44:31 +0300

    Implemented crud for post model with permission and templates

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,13 +77,12 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'your_db_name',
-        'USER': 'your_username',
-        'PASSWORD': 'your_password',
+        'USER': 'your_db_user',
+        'PASSWORD': 'your_db_password',
         'HOST': 'localhost',
         'PORT': '5432',
-    }
 }
-
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
