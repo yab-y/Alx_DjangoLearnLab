05ec0eb4c1ed87b9b30156f691f642a1e545bccc
Yeabsira Wondwossen <yeabwondu48@gmail.com>
2025-08-17 23:10:46 +0300

    add tagging with django_taggit and search functionality

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from .views import search_posts
 from .views import (
     PostListView,
     PostDetailView,
@@ -9,19 +8,26 @@ from .views import (
     CommentCreateView,
     CommentUpdateView,
     CommentDeleteView,
+    search_posts,
+    PostByTagListView,  # import the tag view
 )
 
 urlpatterns = [
-    # Post URLs
-    path('post/', PostListView.as_view(), name='post-list'),                 # list all posts
-    path('post/new/', PostCreateView.as_view(), name='post-create'),         # create a post
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),    # view a single post
-    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # edit post
-    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),  # delete post
+    # Post CRUD
+    path('post/', PostListView.as_view(), name='post-list'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+
+    # Comment CRUD
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+
+    # Search
     path('search/', search_posts, name='search-posts'),
-    path('tags/<slug:tag_name>/', posts_by_tag, name='posts-by-tag'),
-    # Comment URLs
-    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),  # add comment
-    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),     # edit comment
-    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),     # delete comment
+
+    # Tag filtering (this is what the check looks for)
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts-by-tag'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,61 +1,60 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import View
+from django.views.generic import View, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from django.db.models import Q
 from .models import Post, Comment
-from .forms import CommentForm
+from .forms import PostForm, CommentForm
 
-# Add comment to a post
+# ------------------------
+# Post CRUD
+# ------------------------
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'blog/post_form.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('post-list')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# ------------------------
+# Comment CRUD
+# ------------------------
+
 class CommentCreateView(LoginRequiredMixin, View):
-    def post(self, request, pk):  # changed post_pk → pk
-        post = get_object_or_404(Post, pk=pk)
-        form = CommentForm(request.POST)
-        if form.is_valid():
-            comment = form.save(commit=False)
-            comment.author = request.user
-            comment.post = post
-            comment.save()
-        return redirect('post-detail', pk=post.pk)
-
-# Edit a comment
-class CommentUpdateView(LoginRequiredMixin, UserPassesTestMixin, View):
-    def get(self, request, pk):
-        comment = get_object_or_404(Comment, pk=pk)
-        form = CommentForm(instance=comment)
-        return render(request, 'blog/comment_form.html', {'form': form, 'comment': comment})
-
-    def post(self, request, pk):
-        comment = get_object_or_404(Comment, pk=pk)
-        form = CommentForm(request.POST, instance=comment)
-        if form.is_valid():
-            form.save()
-            return redirect('post-detail', pk=comment.post.pk)
-        return render(request, 'blog/comment_form.html', {'form': form, 'comment': comment})
-
-    def test_func(self):
-        comment = get_object_or_404(Comment, pk=self.kwargs['pk'])
-        return self.request.user == comment.author
-
-# Delete a comment
-class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, View):
-    def post(self, request, pk):
-        comment = get_object_or_404(Comment, pk=pk)
-        post_pk = comment.post.pk
-        comment.delete()
-        return redirect('post-detail', pk=post_pk)
-
-    def test_func(self):
-        comment = get_object_or_404(Comment, pk=self.kwargs['pk'])
-        return self.request.user == comment.author
-
-# Search posts
-def search_posts(request):
-    query = request.GET.get('q')
-    results = []
-    if query:
-        results = Post.objects.filter(
-            Q(title__icontains=query) | 
-            Q(content__icontains=query) | 
-            Q(tags__name__icontains=query)
-        ).distinct()
-    return render(request, 'blog/search_results.html', {'results': results, 'query': query})
+    def post(self, request, pk):  # pk = post id
+        post
